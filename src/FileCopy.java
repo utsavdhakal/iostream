5f71e7b5d86cd7de86a9224b86c721ec3f12c500
Utsav Dhakal <udhakal970@gmail.com>
2022-12-08 17:23:03 +0545

    file copying dynamically using scanner approach

--- src/FileCopy.java
+++ src/FileCopy.java
@@ -2,12 +2,21 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileCopy {
     public static void main(String[] args)
             throws FileNotFoundException, IOException {
-        FileInputStream fis = new FileInputStream(args[0]);
-        FileOutputStream fos = new FileOutputStream(args[1]);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter source file: ");
+        String source = sc.nextLine();
+
+        System.out.print("Enter destination file: ");
+        String destination = sc.nextLine();
+
+        FileInputStream fis = new FileInputStream(source);
+        FileOutputStream fos = new FileOutputStream(destination);
 
         int data = fis.read();
         while (data != -1) {
